dbac622cc2513ce23a462bc8c09b086fe6aea4b4
Ashok Dudhade <ashok.dudhade@outlook.com>
2019-04-25 22:42:37 -0500

    Added analytics on share link

--- src/components/Share/index.jsx
+++ src/components/Share/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import './style.scss';
+import { OutboundLink } from 'gatsby-plugin-google-analytics'
+
 
 function Share({ title, url }) {
   const encodedURL = encodeURI(url);
@@ -10,7 +12,7 @@ function Share({ title, url }) {
         <span>Share This Post
         </span>
         <span className="shareIconContainer">
-          <a
+          <OutboundLink
             className="shareButton"
             href={`https://twitter.com/intent/tweet?text=${text}&url=${encodedURL}`}
             target="_blank"
@@ -30,8 +32,8 @@ function Share({ title, url }) {
             >
               <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
             </svg>
-          </a>
-          <a
+          </OutboundLink>
+          <OutboundLink
             className="shareButton"
             href={`https://www.linkedin.com/shareArticle?mini=true&url=${encodedURL}&title=${text}`}
             target="_blank"
@@ -53,7 +55,7 @@ function Share({ title, url }) {
               <rect x="2" y="9" width="4" height="12" />
               <circle cx="4" cy="4" r="2" />
             </svg>
-          </a>
+          </OutboundLink>
         </span>
       </p>
     </div>
